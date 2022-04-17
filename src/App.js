8006d76cc1e298b8f5453230f2e292fe6de2e29d
Nelson <nelsoninho11@gmail.com>
2022-04-17 15:10:54 +0100

    commited main files

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from "react";
+import AppContext from "./AppContext";
+import questionsArray from "./constants/questionsArray";
+import Questions from "./components/Questions";
+import { Typography } from "@material-ui/core";
+
+function App() {
+  let [questions, setQuestions] = useState([]);
+  let [answers, setAnswers] = useState([]);
+  let [questionAnswer, setQuestionAnswer] = useState({});
+
+  useEffect(() => {
+    setQuestions(questionsArray);
+    setQuestionAnswer(questionsArray[0]);
+  }, []);
+
+  let handleChangeInput = (e) => {
+    setQuestionAnswer({
+      ...questionAnswer,
+      answer: e.target.value,
+    });
+  };
+
+  let nextQuestion = (e) => {
+    e.preventDefault();
+    // eslint-disable-next-line
+    questions.map((question) => {
+      if (question.resumeFieldId === questionAnswer.resumeFieldId) {
+        setAnswers([
+          ...answers,
+          { ...question, answer: questionAnswer.answer },
+        ]);
+      }
+    });
+    // eslint-disable-next-line
+    questions.map((qa, index) => {
+      if (index <= questions.length) {
+        if (qa.resumeFieldId === questionAnswer.resumeFieldId) {
+          // eslint-disable-next-line
+          setQuestionAnswer(questions[index + 1]);
+        }
+      }
+    });
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        state: {
+          questionAnswer,
+          questions,
+          answers,
+        },
+        function: {
+          handleChangeInput: handleChangeInput,
+          nextQuestion: nextQuestion,
+        },
+      }}
+    >
+      <div className="App">
+        <Typography
+          variant="h6"
+          style={{
+            textAlign: "center",
+            margin: "2rem",
+          }}
+        >
+          Resume Builder
+        </Typography>
+        <Questions />
+      </div>
+    </AppContext.Provider>
+  );
+}
+
+export default App;
