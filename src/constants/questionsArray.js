6b25bde6dd21a33c4c6d6d28bc79adc70f1e8945
Nelson <nelsoninho11@gmail.com>
2022-04-17 15:55:40 +0100

    Added more questions

--- src/constants/questionsArray.js
+++ src/constants/questionsArray.js
@@ -16,28 +16,55 @@ let questionsArray = [
   },
   {
     question: "Enter your address",
-    resumeField: "Adress",
+    resumeField: "Address",
     resumeFieldId: "address",
   },
   {
-    question: "What is your career objective",
+    question: "Enter career objective",
     resumeField: "Career Objective",
     resumeFieldId: "objective",
   },
   {
-    question: "What are your skills and abilities",
+    question: "What are your skills",
     resumeField: "Skills",
     resumeFieldId: "skills",
   },
+
   {
-    question: "Work experience",
-    resumeField: "Work Experience",
-    resumeFieldId: "experience",
+    question: "Company Name",
+    resumeField: "Company Name",
+    resumeFieldId: "companyName",
   },
   {
-    question: "Education",
-    resumeField: "Educational Qualifications",
-    resumeFieldId: "education",
+    question: "Position Title",
+    resumeField: "Position Title",
+    resumeFieldId: "positionTitle",
+  },
+  {
+    question: "Main Tasks",
+    resumeField: "Main Tasks & Achievements",
+    resumeFieldId: "mainTasks",
+  },
+  {
+    question: "How long have you worked here?",
+    resumeField: "Duration",
+    resumeFieldId: "duration",
+  },
+
+  {
+    question: "School Name",
+    resumeField: "School Name",
+    resumeFieldId: "schoolName",
+  },
+  {
+    question: "Title of Study",
+    resumeField: "Title of Study",
+    resumeFieldId: "titleOfStudy",
+  },
+  {
+    question: "Date of Graduation",
+    resumeField: "Date of Graduation",
+    resumeFieldId: "dateOfGraduation",
   },
 ];
 export default questionsArray;
